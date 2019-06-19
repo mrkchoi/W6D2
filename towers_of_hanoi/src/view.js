0e9a7344106c0e820efa2574fc8bc326942d5249
mrkchoi <kennethichoi@gmail.com>
2019-06-18 17:30:55 -0700

    Render base view for towers_of_hanoi

--- towers_of_hanoi/src/view.js
+++ towers_of_hanoi/src/view.js
@@ -2,14 +2,19 @@ class HanoiView {
   constructor(game, rootEl) {
     this.game = game;
     this.rootEl = rootEl;
+    this.towers = this.towers();
     // this.setupTowers();
     this.render();
     this.bindHandler();
   }
 
-  setupTowers(){
-  //pull from this.game.towers
-  //and assign proper classes
+  // setupTowers(){
+  // //pull from this.game.towers
+  // //and assign proper classes
+  // }
+
+  towers() {
+    return this.game.towers;
   }
 
   bindHandler() {
@@ -20,7 +25,44 @@ class HanoiView {
   }
 
   render(curTower){
-    console.log(curTower);  
+    let towersDOM = [
+      $('.list1 .list__item'), 
+      $('.list2 .list__item'), 
+      $('.list3 .list__item')
+    ];
+
+    towersDOM.forEach(tower => {
+    });
+
+    console.log(this.towers);
+
+    this.towers.forEach((tower, towerIdx) => {
+      tower.forEach((el, idx) => {
+        if (this.towers[towerIdx][idx]) {
+          let towerDOM = towersDOM[towerIdx];
+
+
+          // find lowest possible pos to append to + append
+          let targetTower = $(towerDOM);
+          let lowestOpenPos = 2;
+          for (let i = 2; i >= 0; i--) {
+            let classList = Array.from(targetTower[i].classList);
+            if (!(classList.includes('item__1') || 
+                  classList.includes('item__2') || 
+                  classList.includes('item__3'))) {
+              $(targetTower[i]).addClass(`item__${i + 1}`);
+            }
+            console.log(classList);
+          }
+          
+
+          // create class to append (e.g. 'item__1')
+
+
+        }
+      });
+    });
+
   }
 
 
